acff7ab99f41dcf8159e527d6c8810383c115448
Aditya Koche <130300872+AdityaKoche01@users.noreply.github.com>
2024-01-19 22:26:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 // Iteration 1: Names and Input
+const firstPlayerChoice = "paper";
+const secondPlayerChoice = "rock";
 //
 // Iteration 2: Conditionals
+const firstPossibility = firstPlayerChoice === "paper" && secondPlayerChoice === "rock";
+const secondPossibility = firstPlayerChoice === "rock" && secondPlayerChoice === "scissors";
+const thirdPossibility = firstPlayerChoice === "scissors" && secondPlayerChoice === "paper";
+const fourthPossibility = secondPlayerChoice === "paper" && firstPlayerChoice === "rock";
+const fifthPossibility = secondPlayerChoice === "rock" && firstPlayerChoice === "scissors";
+const sixthPossibility = secondPlayerChoice === "scissors" && firstPlayerChoice === "paper";
+const seventhPossibility = firstPlayerChoice === secondPlayerChoice;
 
 
 // Iteration 3: Loops
+if (firstPossibility || secondPossibility || thirdPossibility) {
+  console.log("First player won!");
+} else if (fourthPossibility || fifthPossibility || sixthPossibility) {
+  console.log("Second player won!");
+} else if (seventhPossibility) {
+  console.log("It's a tie!");
+} else {
+  console.log("Something went wrong!");
+}
